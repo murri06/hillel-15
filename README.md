e6d51d9cc19143f0fa0fe24f38672eef8b89f609
murri06 <ruslanliakhovets@outlook.com>
2023-12-11 17:10:44 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project 15 for Hillel
+Creating default laravel project and adding a new Controller with view
+
+to use it, clone the repo and use command <br>
+`docker-compose up --build`
+
+Made by Ruslan Liakhovets
---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Include Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <h2>Hello from Home view!</h2>
+    <a class="btn btn-dark" href="/">Welcome view</a>
+</div>
+
+</body>
+</html>
